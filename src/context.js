31b47093c99089adb2241d01a34fe86a40c14b81
Daniel Arnauer <wi19b087@technikum-wien.at>
2021-10-15 11:48:45 +0200

    add comments

--- src/context.js
+++ src/context.js
@@ -1,12 +1,14 @@
 // Source: https://itnext.io/react-hooks-with-context-as-a-state-management-solution-526d1c13a07d
 import React, { useReducer } from "react";
 
+// Add default context here
 const initialState = {
   currentPage: "home",
 };
 
 const PageContext = React.createContext(initialState);
 
+// Add new actions here for global context
 const pageReducer = (state, action) => {
   switch (action.type) {
     case "setCurrentPage":
